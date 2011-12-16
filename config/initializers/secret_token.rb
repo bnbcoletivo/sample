42090ba75b37fbd0f42c8d78b65eebe943a0946b
Luiz Filho <bnbcoletivo@gmail.com>
2011-12-15 22:12:54 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coletivo::Application.config.secret_token = 'd17e09fd2bcc0f960a98c858e48d410642d14c33586edc10e29c51ca19f76a85dd4a630ac7043704b8e076fd7079a3b8795ec6748c39df705048e2b935b8a992'
