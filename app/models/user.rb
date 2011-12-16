9d5488bc58b7c07454810cda8f86416a1a3a7c2b
Luiz Filho <bnbcoletivo@gmail.com>
2011-12-15 23:53:19 -0300

    Added active admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   belongs_to :role
   belongs_to :city
+  
+  def to_s
+    self.firstname + " " + self.lastname
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Coletivo::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :users
 
   resources :shops
@@ -60,7 +64,7 @@ Coletivo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
